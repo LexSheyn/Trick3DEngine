5a07e5a659076e90cb4fb2b395aa8a80d06e5645
Lex Sheyn <tiadec@gmail.com>
2022-03-18 21:13:33 +0300

    Removed if statement in T3D_CALL macro to fix the bug.

--- Engine/src/Macros/T3DMacros.h
+++ Engine/src/Macros/T3DMacros.h
@@ -10,12 +10,8 @@
 #define T3D_INLINE inline
 #endif
 
-#if _M_AMD64
+
 #define T3D_CALL __fastcall
-#else
-// Does nothing if not MSC
-#define T3D_CALL
-#endif
 
 
 #define T3D_NO_DISCARD [[nodiscard]]
